57b97394368f2fc9cf4c534d427bfb80094b9cd1
Tito Rivera <titorivera0707@gmail.com>
2023-01-05 20:17:40 -0800

    Initial Commit

---
+++ FirstIOSApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstIOSApp
+//
+//  Created by Tito Rivera on 1/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
